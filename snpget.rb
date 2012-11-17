9232009f1ea94475b90a834e5db16f421b9c10d7
Colin <vogon@icculus.org>
2012-11-16 20:27:04 -0800

    removed some XML files, added caching to snpget

--- snpget.rb
+++ snpget.rb
@@ -4,26 +4,49 @@ require 'stringio'
 
 Bio::NCBI.default_email = "vogon@icculus.org"
 
+def make_cache_filename(rsid)
+	"dbsnp_cache/#{rsid}.xml"
+end
+
 def store_snp_to_cache(rsid, xml)
-	io = open("#{rsid}.xml", "w")
+	io = open(make_cache_filename(rsid), "w")
 	io << xml.to_xml(:indent => 4)
 	io.close
 end
 
-def get_snp(rsid)
+def fetch_snp_from_cache(rsid)
+	filename = make_cache_filename(rsid)
+
+	if File::exists?(filename) then
+		puts "fetching rs#{rsid} from cache..."
+
+		open(filename)
+	else
+		nil
+	end
+end
+
+def fetch_snp_from_entrez(rsid)
+	puts "fetching rs#{rsid} from entrez..."
+
 	ncbi = Bio::NCBI::REST.new
 
 	result = ncbi.efetch(rsid, {"db"=>"snp", "retmode"=>"xml"})
-	result_io = StringIO.new(result)
-
-	Nokogiri::XML(result_io) do |config|
-		config.default_xml.noblanks
-	end
+	StringIO.new(result)
 end
 
 if __FILE__ == $0 then
 	rsid = ARGV[0]
 
-	snp_xml = get_snp(rsid)
+	snp_io = fetch_snp_from_cache(rsid)
+
+	if snp_io.nil? then
+		snp_io = fetch_snp_from_entrez(rsid)
+	end
+
+	snp_xml = Nokogiri::XML(snp_io) do |config|
+		config.default_xml.noblanks
+	end
+
 	store_snp_to_cache(rsid, snp_xml)
 end
