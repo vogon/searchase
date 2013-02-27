bb6ad726b5fbade0067411035ed6f9eb5c72818d
Colin <vogon@icculus.org>
2013-02-27 08:43:10 -0800

    first bit of new SNP object model

--- snp.rb
+++ snp.rb
@@ -2,58 +2,97 @@
 
 require './snp-dbsnp'
 
-class SNP
-	@@snps = {}
+# SNP contains information about the physical existence of a locus of variation
+# (i.e., its chromosome and position)
 
-	def initialize(rsid)
-		self.rsid = rsid
-		@@snps[rsid] = self
+# Allele contains information about a single variation of that locus
+
+# Mapping contains information about the use of the locus in a hypothetical or known
+# gene or noncoding product (i.e., the gene containing the locus, whether the allele owning 
+# the mapping is the reference allele or a mutation, etc)
+
+# Assay contains information about a measurement taken at a locus
+
+# SNP
+# => has-a snp id (rsid or illumina)
+# => has-a chromosome
+# => has-a clinical significance
+#
+# => has-many alleles
+# => has-many assays
+
+SNPs = {}
+
+class SNP
+	def initialize(id)
+		self.id = id
+
+		self.alleles = {}
+		self.assays = {}
+
+		yield self if block_given?
+	end
+
+	attr_accessor :id
+	
+	attr_accessor :chr
+	attr_accessor :clinical_significance
+
+	attr_accessor :alleles
+	attr_accessor :assays
+end
+
+# Allele
+# => has-a sequence
+#
+# => has-many mappings
+
+class Allele
+	def initialize(sequence)
+		self.sequence = sequence
 
 		self.mappings = {}
 
 		yield self if block_given?
 	end
 
-	def self.[](rsid)
-		if @@snps[rsid] then
-			@@snps[rsid]
-		else
-			SNP.load_dbSNP(rsid)
-		end
-	end
-
-	attr_accessor :rsid
-	attr_accessor :chr
-
-	attr_accessor :clinical_significance
-
-	attr_accessor :observed
-	attr_accessor :primary_seq
-	attr_accessor :exemplar_orientation
-	attr_accessor :exemplar_strand
+	attr_accessor :sequence
 
 	attr_accessor :mappings
 end
 
-class Allele
-	def initialize(sequence)
-		self.sequence = sequence
+# Mapping
+# => has-a symbol
+# => has-a entrezgene id
+# => has-a function class
+# => has-a so-term
+
+class Mapping
+	def initialize(id)
+		self.entrezgene_id = id
+
 		yield self if block_given?
 	end
 
-	attr_accessor :sequence
+	attr_accessor :entrezgene_id
+	attr_accessor :symbol
 
 	attr_accessor :function_class
 	attr_accessor :so_term
 end
 
-class Mapping
-	def initialize(gene)
-		self.gene = gene
-		self.alleles = {}
+# Assay
+# => has-a sample id
+# => has-a genotype
+
+class Assay
+	def initialize(sample_id)
+		self.sample_id = sample_id
+
 		yield self if block_given?
 	end
 
-	attr_accessor :gene
-	attr_accessor :alleles
+	attr_accessor :sample_id
+
+	attr_accessor :genotype
 end
